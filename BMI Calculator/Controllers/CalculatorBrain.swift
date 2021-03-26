6a41f7e2fc535fa556d4eff4492d75808436b029
Dmytro Yantsybaiev <d.yantsybaiev@icloud.com>
2021-03-26 21:22:30 +0200

    Now result view has correct a piece of advice and changed background color according to user's result.

--- BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -6,20 +6,38 @@
 //  Copyright © 2021 Angela Yu. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 
 struct CalculatorBrain {
     
-    var bmi: Float?
+    var bmi: BMI?
     
     func getBMIValue() -> String {
-        let bmiTo1DecimalPlace = String(format: "%.1f", bmi ?? -1.0)
+        let bmiTo1DecimalPlace = String(format: "%.1f", bmi?.value ?? -1.0)
         return bmiTo1DecimalPlace
     }
     
+    func getAdvice() -> String {
+        let bmiAdvice = bmi?.advice ?? "Advice has not found"
+        return bmiAdvice
+    }
+    
+    func getColor() -> UIColor {
+        let bmiColor = bmi?.color ?? #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        return bmiColor
+    }
+    
     mutating func calculateBMI(height: Float, weight: Float) {
-        bmi = weight / pow(height, 2)
+        let bmiValue = weight / pow(height, 2)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1))
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1))
+        }
     }
     
 }
